5b620e194abd232bb75bd8d05df5d594a370c8f6
Bruno Boettcher <bboett@gmail.com>
2015-10-08 15:41:49 +0200

    added to maven project , android app project, added dependency to ipxcontrol, doesn't work...

---
+++ src/main/java/com/nohkumado/teleIPX800/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nohkumado.teleIPX800;
+
+import android.app.*;
+import android.os.*;
+import com.nohkumado.ipx800Control.*;
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
