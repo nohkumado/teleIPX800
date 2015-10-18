a21edfc3a517fcef21d580aa86a24e49e19a3108
Bruno Boettcher <nokumado@gmail.com>
2015-10-19 01:07:57 +0200

    Added status class, and now we display the server name in green if we have the connection, red otherwise, works so far...

---
+++ src/main/java/com/nohkumado/teleIPX800/IpxStatus.java
@@ -0,0 +1,307 @@
+package com.nohkumado.teleIPX800;
+import android.content.*;
+import android.preference.*;
+import android.util.*;
+import com.nohkumado.ipx800control.*;
+import java.io.*;
+import java.nio.charset.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.xmlpull.v1.*;
+
+public class IpxStatus
+{
+  private static final String ns = null;
+  protected Ipx800Control ipx = null;
+  protected boolean connected = false;
+  protected boolean leds[] = new boolean[32];
+  protected boolean buttons[] = new boolean[32];
+  protected Date ipxDate;
+  protected int[] analog = new int[16];
+  protected int[] counter = new int[8];
+
+
+  public final static String TAG = "IPXSTATUS";
+
+  private String tinfo;
+
+  private String version;
+
+  protected Pattern ledPat = Pattern.compile("led(\\d+)");
+  protected Pattern butPat = Pattern.compile("btn(\\d+)");
+  protected Pattern anaPat = Pattern.compile("analog(\\d+)");
+  protected Pattern selPat = Pattern.compile("anselect(\\d+)");
+  protected Pattern couPat = Pattern.compile("count(\\d+)");
+
+  
+
+  public IpxStatus(Ipx800Control i)
+  {
+	ipx = i;
+	//refresh();
+  }
+
+  public void refresh(MainActivity context)
+  {
+	if (ipx != null)
+	{
+	  SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+	  if (sp.contains("servername")) ipx.setHost(sp.getString("servername", ipx.getHost()));
+	  else sp.edit().putString("servername", ipx.getHost()).apply();
+	  if (sp.contains("serverport")) ipx.setPort(sp.getInt("serverport", ipx.getPort()));
+	  else sp.edit().putInt("serverport", ipx.getPort()).apply();//else Log.d(TAG, "proceeding with createview");
+	  
+	  String result = ipx.status();
+	  //Log.d(TAG,"ipx returned "+result);
+	  if (result.length() > 0) connected = true;
+	  InputStream stream = new ByteArrayInputStream(result.getBytes(StandardCharsets.UTF_8));
+	  try
+	  {
+		parse(stream);
+	  }
+	  catch (Exception e)
+	  { Log.d(TAG, "something went wrong parsing " + result);}
+	}
+  }
+
+  public boolean isConnected()
+  {
+	return connected;
+  }
+
+  public void parse(InputStream in) throws XmlPullParserException, IOException 
+  {
+	try 
+	{
+	  XmlPullParser parser = Xml.newPullParser();
+	  parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+	  parser.setInput(in, null);
+	  parser.nextTag();
+	  readStatus(parser);
+	} 
+	finally 
+	{
+	  in.close();
+	}
+  }
+
+  private void readStatus(XmlPullParser parser) throws XmlPullParserException, IOException 
+  {
+	Log.d(TAG,"starting parsing "+parser.getName());
+    parser.require(XmlPullParser.START_TAG, ns, "response");
+    while (parser.next() != XmlPullParser.END_TAG) 
+	{
+	  if (parser.getEventType() != XmlPullParser.START_TAG)
+	  {
+		continue;
+	  }
+	  String name = parser.getName();
+	  //Log.d(TAG,"found tag to parse  "+name);
+	  // Starts by looking for the entry tag
+	  if (name.startsWith("led"))
+	  {
+		Matcher match = ledPat.matcher(name);
+		
+		if(match.find())
+		{
+		  int index = Integer.parseInt(match.group(1));
+		  String content = readText(parser);
+		  if (content.equals("1")) leds[index] = true;
+		  else leds[index] = false;
+		}
+		else Log.e(TAG,"couldn't find a number in "+name);
+	  }
+	  else if (name.startsWith("btn"))
+	  {
+		Matcher match = butPat.matcher(name);
+		if(match.find())
+		{
+		int index = Integer.parseInt(match.group(1));
+		String content = readText(parser);
+
+		if (content.equals("UP")) buttons[index] = true;
+		else buttons[index] = false;
+		}
+	  }
+	  else if (name.matches("day")) //   <day>16/10/2015 </day>
+	  {
+		String content = readText(parser);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		ipxDate = new Date();
+		try
+		{
+		  ipxDate = dateFormat.parse(content);
+		}
+		catch (ParseException e)
+		{
+		  // TODO Auto-generated catch block
+		  e.printStackTrace();
+		}  
+	  }
+	  else if (name.startsWith("time")) //   <time0>19:30:59</time0> 
+	  {
+		String content = readText(parser);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+		Date tmpStamp = new Date();
+		try
+		{
+		  tmpStamp = dateFormat.parse(content);
+		  ipxDate.setHours(tmpStamp.getHours());
+		  ipxDate.setMinutes(tmpStamp.getMinutes());
+		  ipxDate.setSeconds(tmpStamp.getSeconds());
+		}
+		catch (ParseException e)
+		{
+		  // TODO Auto-generated catch block
+		  e.printStackTrace();
+		}  
+	  }//<analog0>0</analog0>
+	  else if (name.startsWith("analog"))
+	  {
+		Matcher match = anaPat.matcher(name);
+		if(match.find())
+		{
+		int index = Integer.parseInt(match.group(1));
+		String content = readText(parser);
+		int value = Integer.parseInt(content);
+
+		analog[index] = value;
+		//Log.d(TAG, "set analog " + index + " to " + value);
+		}
+	  }
+	  else if (name.startsWith("anselect")) //<anselect0>0</anselect0>
+	  {
+		Matcher match = selPat.matcher(name);
+		if(match.find())
+		{
+		  
+		int index = Integer.parseInt(match.group(1));
+		String content = readText(parser);
+		//Log.d(TAG, "extracted anselect " + index + " to " + content);
+		/*int value = Integer.parseInt(content);
+		 TODO find out what this is x16
+		 analog[index] = value;
+		 Log.d(TAG, "set analog " + counter + " to " + value);
+		 */
+		}
+	  }
+	  else if (name.startsWith("count")) //<count0>23</count0>
+	  {
+		Matcher match = couPat.matcher(name);
+		if(match.find())
+		{
+		int index = Integer.parseInt(match.group(1));
+		String content = readText(parser);
+		int value = Integer.parseInt(content);
+
+		counter[index] = value;
+		}
+	  }
+	  else if (name.matches("tinfo")) // <tinfo>---</tinfo>
+	  {
+		tinfo = readText(parser);
+	  }
+	  else if (name.matches("version")) //   <version>3.05.59d</version>
+	  {
+		version = readText(parser);
+	  }
+	  else
+	  {
+		skip(parser);
+	  }
+    }  
+  }
+
+  /* 
+   example of parsing sub trees, not needed at the moment....
+   // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
+   // to their respective "read" methods for processing. Otherwise, skips the tag.
+   private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException
+   {
+   parser.require(XmlPullParser.START_TAG, ns, "entry");
+   String title = null;
+   String summary = null;
+   String link = null;
+   while (parser.next() != XmlPullParser.END_TAG)
+   {
+   if (parser.getEventType() != XmlPullParser.START_TAG)
+   {
+   continue;
+   }
+   String name = parser.getName();
+   if (name.equals("title"))
+   {
+   title = readTitle(parser);
+   }
+   else if (name.equals("summary"))
+   {
+   summary = readSummary(parser);
+   }
+   else if (name.equals("link"))
+   {
+   link = readLink(parser);
+   }
+   else
+   {
+   skip(parser);
+   }
+   }
+   return new Entry(title, summary, link);
+   }
+   // Processes link tags in the feed.
+   private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException
+   {
+   String link = "";
+   parser.require(XmlPullParser.START_TAG, ns, "link");
+   String tag = parser.getName();
+   String relType = parser.getAttributeValue(null, "rel");  
+   if (tag.equals("link"))
+   {
+   if (relType.equals("alternate"))
+   {
+   link = parser.getAttributeValue(null, "href");
+   parser.nextTag();
+   } 
+   }
+   parser.require(XmlPullParser.END_TAG, ns, "link");
+   return link;
+   }
+
+   */
+
+// For the tags title and summary, extracts their text values.
+  private String readText(XmlPullParser parser) throws IOException, XmlPullParserException
+  {
+    String result = "";
+    if (parser.next() == XmlPullParser.TEXT)
+	{
+	  result = parser.getText();
+	  parser.nextTag();
+    }
+    return result;
+  }
+  private void skip(XmlPullParser parser) throws XmlPullParserException, IOException
+  {
+    if (parser.getEventType() != XmlPullParser.START_TAG)
+	{
+	  throw new IllegalStateException();
+    }
+    int depth = 1;
+    while (depth != 0)
+	{
+	  switch (parser.next())
+	  {
+        case XmlPullParser.END_TAG:
+		  depth--;
+		  break;
+        case XmlPullParser.START_TAG:
+		  depth++;
+		  break;
+	  }
+    }
+  }
+}
+
--- src/main/java/com/nohkumado/teleIPX800/MainActivity.java
+++ src/main/java/com/nohkumado/teleIPX800/MainActivity.java
@@ -19,7 +19,6 @@ package com.nohkumado.teleIPX800;
  along with @filename.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import android.*;
 import android.app.*;
 import android.content.*;
 import android.graphics.*;
@@ -34,151 +33,175 @@ import com.nohkumado.ipx800control.*;
 
 public class MainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener
 {
-	public final static String TAG ="MA";
-	protected Ipx800Control ipx = new Ipx800Control();
-	protected ButtonClickListener clickHandler = new ButtonClickListener(this);
-	protected ButtonAdapter buttonArray;
+  public final static String TAG ="MA";
+  protected Ipx800Control ipx = new Ipx800Control();
+  protected IpxStatus status ;
+  protected ButtonClickListener clickHandler = new ButtonClickListener(this);
+  protected ButtonAdapter buttonArray;
 
-	/**
-	 getIpx
-	 returns the ipx object
-	 */
-	public Ipx800Control getIpx()
-	{
-		return ipx;
-	}
-	/**
-	 onCreate
-	 creates if needed a new ButtonAdapter
-	 */
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		if (buttonArray == null) buttonArray = new ButtonAdapter(this);
-		buttonArray.clear();
-		//Log.d(TAG, "filling grid");
-		buttonArray.fillData(clickHandler);
-		//Log.d(TAG,"grid? "+findViewById(R.id.shortcutgrid));
-		GridView shortcuts = (GridView) findViewById(R.id.shortcutgrid);
-		if (shortcuts == null)
-		{
-			//Log.d(TAG, "no grid found...");
-			LinearLayout mainWindow = (LinearLayout)findViewById(R.id.maincontainer);
-			//Log.d(TAG,"mainwin "+mainWindow);
-			
-			shortcuts = new GridView(this);
-			shortcuts.setId(mainWindow.generateViewId());
-			shortcuts.setLayoutParams(new GridView.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
-			shortcuts.setBackgroundColor(Color.WHITE);
-			shortcuts.setNumColumns(3);
-			shortcuts.setColumnWidth(GridView.AUTO_FIT);
-			shortcuts.setVerticalSpacing(5);
-			shortcuts.setHorizontalSpacing(5);
-			shortcuts.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
-			mainWindow.addView(shortcuts);
-			//else Log.e(TAG,"couldn't find mainWindow???");
-		}
-		shortcuts.setAdapter(buttonArray);
-	}
+  public MainActivity()
+  {
+	super();
+	status = new IpxStatus(ipx) ;	
+  }
+
+  /**
+   getIpx
+   returns the ipx object
+   */
+  public Ipx800Control getIpx()
+  {
+	return ipx;
+  }
+  /**
+   onCreate
+   creates if needed a new ButtonAdapter
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+	super.onCreate(savedInstanceState);
+	Log.d(TAG, "calling refresh");
+	status.refresh(this);
+	Log.d(TAG, "ipx    status = " + status.isConnected());
+	setContentView(R.layout.main);
+
+	//Log.d(TAG, "stored servername = " + sp.getString("servername", "non"));	  
+	//Log.d(TAG, "ipx    servername = " + ipx.getHost());
 	
-	/**
-	 onCreateView
-	 finds the different fields, fills in the labels etc. triggers the button-grid-creation
-	 */
-	@Override
-	public View onCreateView(View parent, String name, Context context, AttributeSet attrs)
-	{
-		View v =  super.onCreateView(parent, name, context, attrs);
-		//Log.d(TAG, "creating view "+v);
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-		//why oh why?? i had this allready in createView???
-		if (buttonArray == null) buttonArray = new ButtonAdapter(this);
-		if (parent == null)
-		{
-			//Log.e(TAG, "no parentview found....");
-			return v;
-		}
-		if (sp.contains("servername")) ipx.setHost(sp.getString("servername", ipx.getHost()));
-		else sp.edit().putString("servername", ipx.getHost()).apply();
-		if (sp.contains("serverport")) ipx.setPort(sp.getInt("serverport", ipx.getPort()));
-		else sp.edit().putInt("serverport", ipx.getPort()).apply();//else Log.d(TAG, "proceeding with createview");
-		/*if(v == null)
-		 {
-		 Log.e(TAG,"no view found....");
-		 return v;
-		 }*/
-		TextView field = (TextView) parent.findViewById(R.id.servernameValue);
-		if (field != null) field.setText(sp.getString("servername", ipx.getHost()));
-		field = (TextView) parent.findViewById(R.id.portValue);
-		if (field != null) field.setText("" + sp.getInt("serverport", ipx.getPort()));
 
-		ipx.setHost(sp.getString("servername", "NA"));
+	if (buttonArray == null) buttonArray = new ButtonAdapter(this);
+	buttonArray.clear();
+	//Log.d(TAG, "filling grid");
+	buttonArray.fillData(clickHandler);
+	//Log.d(TAG,"grid? "+findViewById(R.id.shortcutgrid));
+	GridView shortcuts = (GridView) findViewById(R.id.shortcutgrid);
+	if (shortcuts == null)
+	{
+	  //Log.d(TAG, "no grid found...");
+	  LinearLayout mainWindow = (LinearLayout)findViewById(R.id.maincontainer);
+	  //Log.d(TAG,"mainwin "+mainWindow);
 
-					/*
-			 or(int i = 0 ; i < NUMBER_OF_IMAGE_BUTTONS; i++){
-			 imageButtons[i] = new ImageButton(this);
-			 imageButtons[i].setImageResource(R.drawable.bola_verde);
-			 imageButtons[i].setLayoutParams(lp);
-			 imageButtons[i].setOnClickListener(mGreenBallOnClickListener);
-			 imageButtons[i].setBackgroundColor(Color.TRANSPARENT); 
-			 imageButtons[i].setTag(i);
-			 imageButtons[i].setId(i);
-			 gameBoard.addView(imageButtons[i]);
-			 }
-			 */
-		
-			
-		//	Log.d(TAG, "no grid found...");
-		//parent.invalidate();
-		//Log.d(TAG, "done creating view ");
-		return v;
+	  shortcuts = new GridView(this);
+	  shortcuts.setId(mainWindow.generateViewId());
+	  shortcuts.setLayoutParams(new GridView.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+	  shortcuts.setBackgroundColor(Color.WHITE);
+	  shortcuts.setNumColumns(3);
+	  shortcuts.setColumnWidth(GridView.AUTO_FIT);
+	  shortcuts.setVerticalSpacing(5);
+	  shortcuts.setHorizontalSpacing(5);
+	  shortcuts.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+	  mainWindow.addView(shortcuts);
+	  //else Log.e(TAG,"couldn't find mainWindow???");
 	}
-	/**
-	 onCreateOptionsMenu
-	 creates the menu atm only the settings
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	shortcuts.setAdapter(buttonArray);
+  }
+
+  /**
+   onCreateView
+   finds the different fields, fills in the labels etc. triggers the button-grid-creation
+   */
+  @Override
+  public View onCreateView(View parent, String name, Context context, AttributeSet attrs)
+  {
+	View v =  super.onCreateView(parent, name, context, attrs);
+	//Log.d(TAG, "creating view "+v);
+	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+	//why oh why?? i had this allready in createView???
+	if (buttonArray == null) buttonArray = new ButtonAdapter(this);
+	if (parent == null)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+	  //Log.e(TAG, "no parentview found....");
+	  return v;
 	}
-	/**
-	 onOptionsItemSelected
-	 star the Preference activity when the settings entry is hit
+	/*if (sp.contains("servername")) ipx.setHost(sp.getString("servername", ipx.getHost()));
+	 else sp.edit().putString("servername", ipx.getHost()).apply();
+	 if (sp.contains("serverport")) ipx.setPort(sp.getInt("serverport", ipx.getPort()));
+	 else sp.edit().putInt("serverport", ipx.getPort()).apply();//else Log.d(TAG, "proceeding with createview");
 	 */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
+	/*if(v == null)
+	 {
+	 Log.e(TAG,"no view found....");
+	 return v;
+	 }*/
+	TextView field = (TextView) parent.findViewById(R.id.servernameValue);
+	if (field != null) 
 	{
-		switch (item.getItemId())
-		{
-            case R.id.menu_item_settings:
-                //Toast.makeText(this, "ADD!", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(this, AppPreferences.class);
-                startActivity(i);
-                return true;
-				case R.id.menu_item_about:
-				FragmentManager fm = getFragmentManager();
-					AboutDialogFragment about = new AboutDialogFragment();
-				about.show(fm,getResources().getString(R.string.about));
-				break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+	  field.setText(sp.getString("servername", ipx.getHost()));
+	  //WTF?? between the CTOR and onCreate this should exist...
+	  //if (status == null) status = new IpxStatus(ipx);
+	  Log.d(TAG, "Setting colors ... ipx connected?? " + status.isConnected());
+	  if (status.isConnected()) field.setTextColor(Color.GREEN);
+	  else field.setTextColor(Color.RED);  
+	}
+	field = (TextView) parent.findViewById(R.id.portValue);
+	if (field != null) field.setText("" + sp.getInt("serverport", ipx.getPort()));
+
+	ipx.setHost(sp.getString("servername", "NA"));
+
+	/*
+	 or(int i = 0 ; i < NUMBER_OF_IMAGE_BUTTONS; i++){
+	 imageButtons[i] = new ImageButton(this);
+	 imageButtons[i].setImageResource(R.drawable.bola_verde);
+	 imageButtons[i].setLayoutParams(lp);
+	 imageButtons[i].setOnClickListener(mGreenBallOnClickListener);
+	 imageButtons[i].setBackgroundColor(Color.TRANSPARENT); 
+	 imageButtons[i].setTag(i);
+	 imageButtons[i].setId(i);
+	 gameBoard.addView(imageButtons[i]);
+	 }
+	 */
+
+
+	//	Log.d(TAG, "no grid found...");
+	//parent.invalidate();
+	//Log.d(TAG, "done creating view ");
+	return v;
+  }
+  /**
+   onCreateOptionsMenu
+   creates the menu atm only the settings
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.menu_main, menu);
+	return true;
+  }
+  /**
+   onOptionsItemSelected
+   star the Preference activity when the settings entry is hit
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+	switch (item.getItemId())
+	{
+	  case R.id.menu_item_settings:
+		//Toast.makeText(this, "ADD!", Toast.LENGTH_SHORT).show();
+		Intent i = new Intent(this, AppPreferences.class);
+		startActivity(i);
 		return true;
+	  case R.id.menu_item_about:
+		FragmentManager fm = getFragmentManager();
+		AboutDialogFragment about = new AboutDialogFragment();
+		about.show(fm, getResources().getString(R.string.about));
+		break;
+	  default:
+		return super.onOptionsItemSelected(item);
 	}
-	/**
-	 onSharedPreferenceChanged
-	 force the redraw of the grid on change of settings
-	 */
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences p1, String p2)
-	{
-		GridView shortcuts = (GridView) findViewById(R.id.shortcutgrid);
-		if (shortcuts != null)shortcuts.invalidate();
-	}
-	
+	return true;
+  }
+  /**
+   onSharedPreferenceChanged
+   force the redraw of the grid on change of settings
+   */
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences p1, String p2)
+  {
+	GridView shortcuts = (GridView) findViewById(R.id.shortcutgrid);
+	if (shortcuts != null)shortcuts.invalidate();
+  }
+
 }
